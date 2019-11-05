773c31abd892ffd6c1071d6fd38b72646039a0e4
Kuran <kurankumar@gmail.com>
2019-11-05 15:50:18 -0500

    Done.

--- solution.rb
+++ solution.rb
@@ -10,6 +10,7 @@ class Deck
     ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
 
+    #Nested each loop to add ranks to all suites
     suits.each do |suit|
       ranks.each do |rank|
         @cards << Card.new(suit, rank)
